2351bd255c8e178adda6de74c5ed02d709893657
kbsniquer <65562500+kbsniquer@users.noreply.github.com>
2020-07-12 15:30:25 -0400

    Add files via upload

---
+++ precache-manifest.6b4da52bd31443845c0dca72e5f6f38e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9792a8720134e623c923a837f491eeee",
+    "url": "/ksniquer/WEB215/Lesson14/index.html"
+  },
+  {
+    "revision": "c84e129cc613da218e83",
+    "url": "/ksniquer/WEB215/Lesson14/static/css/main.d29a5baa.chunk.css"
+  },
+  {
+    "revision": "35676c632173b01fc9e0",
+    "url": "/ksniquer/WEB215/Lesson14/static/js/2.f941cdbe.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/ksniquer/WEB215/Lesson14/static/js/2.f941cdbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84e129cc613da218e83",
+    "url": "/ksniquer/WEB215/Lesson14/static/js/main.8ae3c0e5.chunk.js"
+  },
+  {
+    "revision": "6fcdafe68c40babe5878",
+    "url": "/ksniquer/WEB215/Lesson14/static/js/runtime-main.4dce6b32.js"
+  },
+  {
+    "revision": "0c55f3c9d91b0524cb96cc1dac2f1f7f",
+    "url": "/ksniquer/WEB215/Lesson14/static/media/Karen.0c55f3c9.png"
+  }
+]);
